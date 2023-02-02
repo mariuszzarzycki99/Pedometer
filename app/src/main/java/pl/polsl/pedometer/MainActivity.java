cfa0a06bca6d17cf6cba7e6c9470482b8af1a3a8
Mariusz Zarzycki <mariuszzarzycki99@gmail.com>
2023-02-02 14:40:18 +0100

    Bottom menu and fragments

--- app/src/main/java/pl/polsl/pedometer/MainActivity.java
+++ app/src/main/java/pl/polsl/pedometer/MainActivity.java
@@ -1,13 +1,23 @@
 package pl.polsl.pedometer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener{
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    SummaryFragment summaryFragment = new SummaryFragment();
+    AchievementsFragment achievementsFragment = new AchievementsFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
 
     @SuppressLint("ResourceType")
     @Override
@@ -15,5 +25,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.homeButton);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.homeButton:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,homeFragment).commit();
+                return true;
+            case R.id.summaryButton:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,summaryFragment).commit();
+                return true;
+            case R.id.achievementsButton:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,achievementsFragment).commit();
+                return true;
+            case R.id.settingsButton:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,settingsFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
