e724144db289f8e9ada8b0791bbe23cd4f5edf40
Mariusz Zarzycki <mariuszzarzycki99@gmail.com>
2023-02-02 14:02:10 +0100

    Init

---
+++ app/src/main/java/pl/polsl/pedometer/MainActivity.java
@@ -0,0 +1,19 @@
+package pl.polsl.pedometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+    }
+}
