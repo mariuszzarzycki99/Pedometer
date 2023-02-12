a9205c01b40594642bd0268505649d4cf4c031ea
jakl115 <43808301+jakl115@users.noreply.github.com>
2023-02-12 19:25:45 +0100

    Summary screen done

--- app/src/main/java/pl/polsl/pedometer/SummaryFragment.java
+++ app/src/main/java/pl/polsl/pedometer/SummaryFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
@@ -16,8 +17,10 @@ import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +30,9 @@ import java.util.List;
 public class SummaryFragment extends Fragment {
     BarChart barChart;
     private static final String historyFile = "pedometerHistory.txt";
+    private List<DateSteps> history;
+
+    private TextView infoTextView;
 
     public SummaryFragment() {
         // Required empty public constructor
@@ -39,6 +45,8 @@ public class SummaryFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getHistory();
+
     }
 
     @Override
@@ -48,21 +56,48 @@ public class SummaryFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_summary, container, false);
 
         barChart = view.findViewById(R.id.barChart);
+        infoTextView = (TextView) view.findViewById(R.id.info);
 
         BarDataSet barDataSet = createGraphData();
         barChart.setData(new BarData(barDataSet));
-        barChart.animateY(1000);
-        barChart.getDescription().setText("Weekly steps");
+        barChart.animateY(500);
+        barChart.getDescription().setText("Last 7 days");
+        barChart.setExtraTopOffset(10.0f);
+        barChart.setExtraBottomOffset(10.0f);
+        barChart.setExtraLeftOffset(10.0f);
+        barChart.setExtraRightOffset(10.0f);
 
         return view;
     }
 
+
+    private void getHistory() {
+        history = ((MainActivity) getActivity()).loadHistory();
+    }
     @NonNull
     private BarDataSet createGraphData() {
         List<BarEntry> barEntries = new ArrayList<>();
-        for(int i = 0; i < 7; i++) {
-            barEntries.add(new BarEntry(i, i));
+
+        int noOfDaysToShow = 7;
+
+        List<DateSteps> lastSevenDays = history.stream()
+                .filter(x -> x.getDate().isAfter(LocalDate.now().minusDays(noOfDaysToShow)))
+                .collect(Collectors.toList());
+
+        for(int i = 0; i < noOfDaysToShow; i++) {
+            LocalDate date = LocalDate.now().minusDays(noOfDaysToShow - i - 1);
+            for(DateSteps day : lastSevenDays) {
+                if(date.equals(day.getDate())) {
+                    barEntries.add(new BarEntry(date.getDayOfMonth(), day.getSteps()));
+                } else {
+                    barEntries.add(new BarEntry(date.getDayOfMonth(), 0));
+                }
+            }
         }
+        String infoText = "From " + LocalDate.now().minusDays(noOfDaysToShow - 1).toString()
+                + " to " + LocalDate.now().toString();
+        infoTextView.setText(infoText);
+
         BarDataSet barDataSet = new BarDataSet(barEntries, "Steps");
         final int[] GRAPH_COLORS = {rgb("#000000")};
         barDataSet.setColors(GRAPH_COLORS);
@@ -74,7 +109,7 @@ public class SummaryFragment extends Fragment {
         int color = (int) Long.parseLong(hex.replace("#", ""), 16);
         int r = (color >> 16) & 0xFF;
         int g = (color >> 8) & 0xFF;
-        int b = (color >> 0) & 0xFF;
+        int b = (color) & 0xFF;
         return Color.rgb(r, g, b);
     }
 }
